25520a6b11737e37240fea7bd1140b5d6d4b7cce
maroder <no-reply@provider.tld>
2020-11-14 05:47:07 -1100

    enhancement of exception name

--- src/Exception.ts
+++ src/Exception.ts
@@ -2,38 +2,38 @@
  * Exception Area
  */
 export class Exception extends Error{
-    public name = Exception["name"];
+    public name = Exception.class().getName();
     constructor( message : string = null, code : number = 0 ) {super(message);}
 }
 export class RuntimeException extends Error{
-    public name = RuntimeException["name"];
+    public name = RuntimeException.class().getName();
     constructor( message : string = null, code : number = 0 ) {super(message);}
 }
 export class IndexOfBoundException extends Error{
-    public name = IndexOfBoundException["name"];
+    public name = IndexOfBoundException.class().getName();
     constructor( message : string = null ) {super(message);}
 }
 export class NullPointerException extends Error{
-    public name = NullPointerException["name"];
+    public name = NullPointerException.class().getName();
     constructor( message : string = null ) {super(message);}
 }
 export class NoSuchElementException extends Error{
-    public name = NoSuchElementException["name"];
+    public name = NoSuchElementException.class().getName();
     constructor( message : string = null ) {super(message);}
 }
 export class IOException extends Error{
-    public name = IOException["name"];
+    public name = IOException.class().getName();
     constructor( message : string = null ) {super(message);}
 }
 export class JSONException extends Error{
-    public name = IOException["name"];
+    public name = IOException.class().getName();
     constructor( message : string = null ) {super(message);}
 }
 export class SecurityException extends Error{
-    public name = SecurityException["name"];
+    public name = SecurityException.class().getName();
     constructor( message : string = null, code : number = 0 ) {super(message);}
 }
 export class ClassNotFoundException extends Error{
-    public name = ClassNotFoundException["name"];
+    public name = ClassNotFoundException.class().getName();
     constructor( message : string = null, code : number = 0 ) {super(message);}
 }
