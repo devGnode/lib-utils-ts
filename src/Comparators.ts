7440b7eee71adcab7f9da11aa2373f16b1c09b56
maroder <no-reply@provider.tld>
2021-06-27 05:52:02 -1100

    refactoring Comparators

--- src/Comparators.ts
+++ src/Comparators.ts
@@ -2,11 +2,36 @@ import {Define} from "./Define";
 import {comparable, comparator} from "./Interface";
 import {Comparator} from "./Comparator";
 import {RuntimeException} from "./Exception";
+import {Collection} from "./Collection";
+/*
+ * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 /****
  *
  */
 type reversed       = { new<T>(compare: comparator<T>): comparator<T> };
-type NullComparator = { new<T>(nullFirst:boolean, comparator: comparator<T>): comparator<T> };
+type NullComparator<T> = {  new<T>(nullFirst:boolean, comparator: comparator<T>): comparator<T> };
 /****
  *
  */
@@ -14,7 +39,7 @@ export abstract class Comparators<T> {
     /****
      *
      */
-    private constructor() { throw new RuntimeException("no usage"); }
+    private constructor() { throw new RuntimeException("no usage inside !"); }
     /****
      *
      */
@@ -32,7 +57,7 @@ export abstract class Comparators<T> {
      *
      */
     public static naturalOrder:comparator<comparable<Object>> = new class implements comparator<comparable<Object>>{
-        // @override
+
         public compare(o1: comparable<Object>, o2: comparable<Object>): number {
             return o1.compareTo(o2);
         }
