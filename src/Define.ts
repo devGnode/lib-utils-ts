9d9eff97871456b888e92bc9deda577c9b8c5d4b
maroder <no-reply@provider.tld>
2021-08-10 08:37:24 -1100

    Refactoring v3 - Optional / Define + test

--- src/Define.ts
+++ src/Define.ts
@@ -1,60 +1,48 @@
-/***
- *
- */
-import {IDefine} from "./Interface";
+import {define} from "./Interface";
 import {Optional} from "./Optional";
 /***
- *
+ * @class Define<T>
+ * @extends Optional<T>
+ * @interface define<T>
+ * Check nullable value
+ * isNull return true when obj is equals to undefined or null
+ * older class v1.0.0, before Optional implementation,
+ * stay here for upward compatibility of my dev.
  */
-export class Define<T> extends Optional<T> implements IDefine<T>{
+export class Define<T> extends Optional<T> implements define<T>{
     /***
      *
      * @param value
      */
     constructor(value : T) { super(value); }
-    /***
-     *
-     */
+    /***@deprecated*/
     public isNullable(): boolean{ return this.isNull();}
     /***
      *
      */
-    public isNull(): boolean{return this.value===null||this.value===undefined;}
+    public isNull(): boolean{return super.isPresent();}
     /***
      *
      * @param value
      */
-    public orNull( value : T ): T{return this.isNull()?value:this.value;}
-    /***
-     *
-     * @param exception
-     */
-    public orElseThrow( exception : Error|TypeError ): T{
-        if( this.isNull() ) throw  exception;
-        return this.value;
-    }
+    public orNull( value : T ): T{return super.orElse(value); }
     /***
      *
      */
-    public orThrow( exception : Object ): Define<T>{
-        if(this.isNull()) throw exception;
+    public orThrow( exception : Error ): Define<T>{
+        if(this.isNull()) this.orElseThrow(exception);
         return this;
     }
     /***
-     *
      */
-    public getType( ){ return typeof this.value; }
+    public getType( ):string{ return typeof this.value; }
     /***
-     *
      */
     public valueOf( ):T{ return this.value; }
     /***
-     *
      */
-    public toString( ) :string{return String(this.value);}
+    public toString( ) :string{return this.value.toString(); }
     /***
-     *
-     * @param value
      */
     public static of<T>( value : T ): Define<T>{return new Define<T>(value);}
 }
