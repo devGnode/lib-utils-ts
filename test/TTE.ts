d29dfc58492c94552dfd20267b9478c27356955b
maroder <no-reply@provider.tld>
2022-03-19 21:13:34 +0100

    Remove test directory

--- test/TTE.ts
+++
@@ -1,8 +0,0 @@
-import { Package } from "../src/Package/Package";
-import {TT} from "./TT";
-console.log("ERORORO-TTE");
-export class TTE extends TT{
-    constructor() {super();}
-}
-
-Package(this);
